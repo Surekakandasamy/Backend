869fed863b1d99a3ab9f91b140ff5be025415c7d
Surekakandasamy <surekakandasamy@example.com>
2025-12-18 13:52:05 +0530

    Hardcode JWT expiration to fix deployment issue

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
+    expiresIn: '7d'
   });
 };
 
